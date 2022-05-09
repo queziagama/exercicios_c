2e8fcac07ed50fa4dd8c484809e9737f9f647afb
queziagama <85850276+queziagama@users.noreply.github.com>
2022-05-09 16:21:08 -0300

    Create for.c

---
+++ for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+
+{   int i;
+    for(i = 99 ; i > 48 ; i--)
+    {printf("%d ",i);
+    }
+}
+
+// pode usar o gdb online para rodar o código 
+// saída esperada: 99 98 97 (até) 51 50 49
